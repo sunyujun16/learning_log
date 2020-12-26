48d64a5d88b2c86ce5a4b4c8d557e9d3334adfd0
sun <sunyujun16@163.com>
2020-12-26 22:25:30 +0800

    manually reference jquery from static dir

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -125,7 +125,9 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
 
 # 我的设置
 LOGIN_URL = '/users/login/'
